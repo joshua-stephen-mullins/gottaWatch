2935e96b913404b03f518eb6c7b470b955fde98f
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-12-12 16:51:53 -0600

    reformatted small cards

--- js/anime-project.js
+++ js/anime-project.js
@@ -37,80 +37,78 @@ $(document).ready(() => {
             // .then(response => console.log('Results by id', response)
             .then((data) => {
                 console.log(data);
-                if (data.hasOwnProperty('title')) {
-                    $(`#moreInfoTitle`).html(data.title)
-                } else {
-                    $(`#moreInfoTitle`).html(data.name)
-                }
-                $('#moreInfoPoster').attr('src', `https://image.tmdb.org/t/p/original/${data.poster_path}`);
-                $('#moreInfoOverview').html(data.overview);
-                if (data.hasOwnProperty('release_date')) {
-                    $(`#moreInfoYear`).html("(" + data.release_date.slice(0, 4) + ")")
-                } else {
-                    $(`#moreInfoYear`).html("(" + data.last_air_date.slice(0, 4) + ")")
-                }
-                $('#moreInfoGenre').html('');
-                for (let i = 0; i < data.genres.length; i++) {
-                    if (i === (data.genres.length - 1)) {
-                        $('#moreInfoGenre').append(`${data.genres[i].name}`)
-                    } else {
-                        $('#moreInfoGenre').append(data.genres[i].name + ', &nbsp;')
-                    }
-                }
-                if (data.hasOwnProperty('runtime')) {
-                    $('#moreInfoRuntime').html(toHoursAndMinutes(data.runtime))
-                } else {
-                    $('#moreInfoRuntime').html(toHoursAndMinutes(data.last_episode_to_air.runtime))
-                }
-                if (searchType === 'movie') {
-                    fetch(`https://api.themoviedb.org/3/movie/${id}/release_dates?api_key=${apiKeyTMDP}`)
-                        .then(response => response.json())
-                        // .then(response => console.log('Results by id', response)
-                        .then((data) => {
-                            console.log(data);
-                            data.results.forEach(function (country) {
-                                if (country.iso_3166_1 === "US") {
-                                    country.release_dates.forEach(function (result) {
-                                        if (result.certification !== '') {
-                                            $('#moreInfoRating').html(result.certification);
-                                        }
-                                    })
-                                }
-                            })
-                        })
-                } else {
-                    fetch(`https://api.themoviedb.org/3/tv/${id}/content_ratings?api_key=${apiKeyTMDP}&language=en-US`)
-                        .then(response => response.json())
-                        // .then(response => console.log('Results by id', response)
-                        .then((data) => {
-                            console.log(data);
-                            data.results.forEach(function (country) {
-                                if (country.iso_3166_1 === "US") {
-                                    $('#moreInfoRating').html(country.rating);
-                                }
-                            })
-                        })
-                }
+                moreInfo(data, searchType, id);
             })
             .catch(err => console.error(err));
+    }
+
+    function moreInfo(data, searchType, id) {
+        if (data.hasOwnProperty('title')) {
+            $(`#moreInfoTitle`).html(data.title)
+        } else {
+            $(`#moreInfoTitle`).html(data.name)
+        }
+        $('#moreInfoPoster').attr('src', `https://image.tmdb.org/t/p/original/${data.poster_path}`);
+        $('#moreInfoOverview').html(data.overview);
+        if (data.hasOwnProperty('release_date')) {
+            $(`#moreInfoYear`).html("(" + data.release_date.slice(0, 4) + ")")
+        } else {
+            $(`#moreInfoYear`).html("(" + data.last_air_date.slice(0, 4) + ")")
+        }
+        $('#moreInfoGenre').html('');
+        for (let i = 0; i < data.genres.length; i++) {
+            if (i === (data.genres.length - 1)) {
+                $('#moreInfoGenre').append(`${data.genres[i].name}`)
+            } else {
+                $('#moreInfoGenre').append(data.genres[i].name + ', &nbsp;')
+            }
+        }
+        if (data.hasOwnProperty('runtime')) {
+            $('#moreInfoRuntime').html(toHoursAndMinutes(data.runtime))
+        } else {
+            $('#moreInfoRuntime').html(toHoursAndMinutes(data.last_episode_to_air.runtime))
+        }
+        if (searchType === 'movie') {
+            fetch(`https://api.themoviedb.org/3/movie/${id}/release_dates?api_key=${apiKeyTMDP}`)
+                .then(response => response.json())
+                // .then(response => console.log('Results by id', response)
+                .then((data) => {
+                    console.log(data);
+                    data.results.forEach(function (country) {
+                        if (country.iso_3166_1 === "US") {
+                            country.release_dates.forEach(function (result) {
+                                if (result.certification !== '') {
+                                    $('#moreInfoRating').html(result.certification);
+                                }
+                            })
+                        }
+                    })
+                })
+        } else {
+            fetch(`https://api.themoviedb.org/3/tv/${id}/content_ratings?api_key=${apiKeyTMDP}&language=en-US`)
+                .then(response => response.json())
+                // .then(response => console.log('Results by id', response)
+                .then((data) => {
+                    console.log(data);
+                    data.results.forEach(function (country) {
+                        if (country.iso_3166_1 === "US") {
+                            $('#moreInfoRating').html(country.rating);
+                        }
+                    })
+                })
+        }
     }
 
     function generateSmallCards(showInfo, numberOfCards, container) {
         for (let i = 0; i < numberOfCards; i++) {
             $(container).append(`
-                <div class="card text-white bg-primary mb-3 col-2" id="showCard${i}" data-type="${showInfo.results[i].media_type} data-showId="${showInfo.results[i].id}">
+                <div class="card border-0 p-0 text-white bg-primary m-3 mb-3 col-1" id="showCard${i}" data-type="${showInfo.results[i].media_type} data-showId="${showInfo.results[i].id}">
                     <div class="">
-                        <button data-bs-toggle="modal" data-bs-target="#moreInfoModal"><img class="w-100 h-100" src="https://image.tmdb.org/t/p/original/${showInfo.results[i].poster_path}" alt="Poster"></button>
+                        <img class="w-100 h-100" src="https://image.tmdb.org/t/p/original/${showInfo.results[i].poster_path}" alt="Poster" data-bs-toggle="modal" data-bs-target="#moreInfoModal">
                     </div>
                     <div class="card-footer">
                         <h5><span id="resultTitle_${showInfo.results[i].id}"></span> <span class="text-muted" id="resultDate_${showInfo.results[i].id}"></span></h5>
                         <p><span id="previewGenre${i}"></span></p>
-                        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                          <input type="radio" class="btn-check " name="btnradio" id="btnradio1" autocomplete="off" checked="">
-                          <label class="btn btn-outline-danger fs-3" for="btnradio1"><i class="fa-solid fa-fire"></i></label>
-                          <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off" checked="">
-                          <label class="btn btn-outline-danger fs-3" for="btnradio2"><i class="fa-solid fa-eye"></i></label>
-                        </div>
                     </div>
                 </div>
             `);
@@ -119,8 +117,10 @@ $(document).ready(() => {
             } else {
                 $(`#resultTitle_${showInfo.results[i].id}`).html(showInfo.results[i].name)
             }
-            $(`#showCard${i}`).click(() => searchById(showInfo.results[i].media_type, showInfo.results[i].id));
-            console.log(showInfo.results[i].release_date);
+            $(`#showCard${i}`).click(() => {
+                searchById(showInfo.results[i].media_type, showInfo.results[i].id);
+                console.log(showInfo.results[i].release_date);
+            })
             if (showInfo.results[i].hasOwnProperty('release_date')) {
                 $(`#resultDate_${showInfo.results[i].id}`).html("(" + showInfo.results[i].release_date.slice(0, 4) + ")")
             } else {
@@ -190,3 +190,34 @@ $(document).ready(() => {
         }
     }
 })
+
+
+// < div
+// className = "btn-group"
+// role = "group"
+// aria - label = "Basic radio toggle button group" >
+//     < input
+// type = "radio"
+// className = "btn-check "
+// name = "btnradio"
+// id = "btnradio1"
+// autoComplete = "off"
+// checked = "" >
+//     < label
+// className = "btn btn-outline-danger fs-3"
+// htmlFor = "btnradio1" > < i
+// className = "fa-solid fa-fire" > < /i></
+// label >
+// < input
+// type = "radio"
+// className = "btn-check"
+// name = "btnradio"
+// id = "btnradio2"
+// autoComplete = "off"
+// checked = "" >
+//     < label
+// className = "btn btn-outline-danger fs-3"
+// htmlFor = "btnradio2" > < i
+// className = "fa-solid fa-eye" > < /i></
+// label >
+// < /div>

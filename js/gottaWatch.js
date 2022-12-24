b6309903f1c9b4538bf2665a750092c6d966a893
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2022-12-23 15:03:00 -1000

    working on autopopulation of lists cards

--- js/gottaWatch.js
+++ js/gottaWatch.js
@@ -368,20 +368,33 @@ $(document).ready(() => {
               </div>
             </div>
         `)
-        // for (let i = 0; i < 6; i++) {
-        //     console.log(list.content[i]);
-        //     fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apiKeyTMDP}&language=en-US&query=${list.content[i]}&page=1&include_adult=false`)
-        //         .then(response => response.json())
-        //         .then((data) => {
-        //             console.log(data);
-        //         })
-        // }
+        if (list.content < 6) {
+            for (let i = 0; i < list.content.length; i++) {
+                console.log(list.content[i]);
+                fetch(`https://api.themoviedb.org/3/${list.content[i].type}/${list.content[i].id}?api_key=${apiKeyTMDP}&language=en-US`)
+                    .then(response => response.json())
+                    .then((data) => {
+                        console.log(data);
+                    })
+            }
+        } else {
+            for (let i = 0; i < 6; i++) {
+                console.log(list.content[i]);
+                fetch(`https://api.themoviedb.org/3/${list.content[i].type}/${list.content[i].id}?api_key=${apiKeyTMDP}&language=en-US`)
+                    .then(response => response.json())
+                    .then((data) => {
+                        console.log(data);
+                    })
+            }
+        }
     }
 
-    // <img src="..." className="img-fluid rounded-start" alt="...">
+    //
 
-    function generatePopularListsCards(list) {
-        $('#popularListsContainer').append(`
+// <img src="..." className="img-fluid rounded-start" alt="...">
+
+function generatePopularListsCards(list) {
+    $('#popularListsContainer').append(`
             <div class="card mb-3 col-6 border border-1" style="max-width: 540px;">
               <div class="row g-0">
                 <div class="col-6">
@@ -396,61 +409,61 @@ $(document).ready(() => {
               </div>
             </div>
         `)
-    }
+}
 
-    function toHoursAndMinutes(totalMinutes) {
-        const hours = Math.floor(totalMinutes / 60);
-        const minutes = totalMinutes % 60;
-        if (hours > 0) {
-            return `${hours}h${minutes > 0 ? ` ${minutes}m` : ''}`;
-        } else
-            return ` ${minutes}m`;
-    }
+function toHoursAndMinutes(totalMinutes) {
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    if (hours > 0) {
+        return `${hours}h${minutes > 0 ? ` ${minutes}m` : ''}`;
+    } else
+        return ` ${minutes}m`;
+}
 
-    function genreIdToText(id) {
-        let genres = [
-            {id: 28, name: 'Action'},
-            {id: 12, name: 'Adventure'},
-            {id: 16, name: 'Animation'},
-            {id: 35, name: 'Comedy'},
-            {id: 80, name: 'Crime'},
-            {id: 99, name: 'Documentary'},
-            {id: 18, name: 'Drama'},
-            {id: 10751, name: 'Family'},
-            {id: 14, name: 'Fantasy'},
-            {id: 36, name: 'History'},
-            {id: 27, name: 'Horror'},
-            {id: 10402, name: 'Music'},
-            {id: 9648, name: 'Mystery'},
-            {id: 10749, name: 'Romance'},
-            {id: 878, name: 'Science Fiction'},
-            {id: 10770, name: 'TV Movie'},
-            {id: 53, name: 'Thriller'},
-            {id: 10752, name: 'War'},
-            {id: 37, name: 'Western'},
-            {id: 10759, name: 'Action & Adventure'},
-            {id: 16, name: 'Animation'},
-            {id: 35, name: 'Comedy'},
-            {id: 80, name: 'Crime'},
-            {id: 99, name: 'Documentary'},
-            {id: 18, name: 'Drama'},
-            {id: 10751, name: 'Family'},
-            {id: 10762, name: 'Kids'},
-            {id: 9648, name: 'Mystery'},
-            {id: 10763, name: 'News'},
-            {id: 10764, name: 'Reality'},
-            {id: 10765, name: 'Sci-Fi & Fantasy'},
-            {id: 10766, name: 'Soap'},
-            {id: 10767, name: 'Talk'},
-            {id: 10768, name: 'War & Politics'},
-            {id: 37, name: 'Western'}
-        ];
-        for (let i = 0; i < genres.length; i++) {
-            if (id === genres[i].id) {
-                return genres[i].name;
-            }
+function genreIdToText(id) {
+    let genres = [
+        {id: 28, name: 'Action'},
+        {id: 12, name: 'Adventure'},
+        {id: 16, name: 'Animation'},
+        {id: 35, name: 'Comedy'},
+        {id: 80, name: 'Crime'},
+        {id: 99, name: 'Documentary'},
+        {id: 18, name: 'Drama'},
+        {id: 10751, name: 'Family'},
+        {id: 14, name: 'Fantasy'},
+        {id: 36, name: 'History'},
+        {id: 27, name: 'Horror'},
+        {id: 10402, name: 'Music'},
+        {id: 9648, name: 'Mystery'},
+        {id: 10749, name: 'Romance'},
+        {id: 878, name: 'Science Fiction'},
+        {id: 10770, name: 'TV Movie'},
+        {id: 53, name: 'Thriller'},
+        {id: 10752, name: 'War'},
+        {id: 37, name: 'Western'},
+        {id: 10759, name: 'Action & Adventure'},
+        {id: 16, name: 'Animation'},
+        {id: 35, name: 'Comedy'},
+        {id: 80, name: 'Crime'},
+        {id: 99, name: 'Documentary'},
+        {id: 18, name: 'Drama'},
+        {id: 10751, name: 'Family'},
+        {id: 10762, name: 'Kids'},
+        {id: 9648, name: 'Mystery'},
+        {id: 10763, name: 'News'},
+        {id: 10764, name: 'Reality'},
+        {id: 10765, name: 'Sci-Fi & Fantasy'},
+        {id: 10766, name: 'Soap'},
+        {id: 10767, name: 'Talk'},
+        {id: 10768, name: 'War & Politics'},
+        {id: 37, name: 'Western'}
+    ];
+    for (let i = 0; i < genres.length; i++) {
+        if (id === genres[i].id) {
+            return genres[i].name;
         }
     }
+}
 })
 
 
